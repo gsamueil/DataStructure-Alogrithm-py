6a889f21f331e65e5830ac77aad1a90925dbd6b9
gsamuel <Gsamuel.it@outlook.com>
2023-02-21 22:14:25 +0200

    edit function

--- Recursion/DecimalToBinary.py
+++ Recursion/DecimalToBinary.py
@@ -31,6 +31,7 @@ def decimalToBinary(n):
     if n == 0:
         return 0
     else:
+    
 
         return n%2 +10* decimalToBinary(int(n/2))
-print(decimalToBinary(10))
+print(decimalToBinary(100))
