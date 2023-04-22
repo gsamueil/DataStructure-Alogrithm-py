1e02ca37e80bd4c5f0530f5f37cbab51556ceacf
gsamuel <Gsamuel.it@outlook.com>
2023-04-23 01:26:17 +0200

    tuples

---
+++ Tuple/tuple.py
@@ -0,0 +1,5 @@
+""" tuples is asequence of values much like a list . the value stored in tuple  can be any type and they are indexed by integers ."""
+newTuple = ("a", "b", "c", "d", "e")
+print(newTuple[1])
+print(newTuple[:])
+print(newTuple[1:3])
