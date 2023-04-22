68c1998a402212c6bcf378358612fda0d995e0bd
gsamuel <Gsamuel.it@outlook.com>
2023-04-22 14:14:55 +0200

    all function in dict

--- Dictionaries/create_dict.py
+++ Dictionaries/create_dict.py
@@ -11,5 +11,22 @@ def traverseDict(dict):
         print(key, dict[key])
 
 
-traverseDict(myDict)
-# s
+# traverseDict(myDict)
+# search in dict
+def searchDict(dict, value):
+    for key in dict:
+        if dict[key] == value:
+            return key, value
+    return "Not exist"
+
+
+print(searchDict(myDict, 39))
+print(searchDict(myDict, 10))
+# to remove item in dict we will used pop function
+myDict.pop("name")
+# if you need to see what you delete you must add print(myDict.pop("name"))
+# to delete specific item we can used popitem
+# if we need to delete all items we can used clear function
+# also we can used del mydict['key name']
+# del mydict   to delete every thing
+print(myDict)
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * 2-Recursion interview questions
 * 3- Big-O Notation
 * 4- Array
+* 5
